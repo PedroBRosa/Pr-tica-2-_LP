63b78ca29e1cedff1d2fd3fd6fce1cebd61d9811
Pedro Rosa <pedro.brosa01@gmail.com>
2022-03-17 16:13:00 -0300

    :fire:
    Atualização da atividade

--- Pr-tica-2-_LP/src/Ex2/Main.java
+++ Pr-tica-2-_LP/src/Ex2/Main.java
@@ -2,18 +2,22 @@ package Ex2;
 
 public class Main {
     public static void main(String[] args) {
+        int somaDP=0;
         int impar=1;
-        int [][] matriz = new int [3][3];
-    for(int i = 0;i< 3;i++){
-        for (int j = 0;j < 3;j++){
-            matriz[i][j]=impar;
-            impar +=2;
-            if(i==j){System.out.println(matriz[i][j]);}
-            if(i!=j){
-                matriz[i][j] = 0;
-                System.out.println(matriz[i][j]);
+        int [][] matriz = new int [50][50];
+    for(int i = 0;i< matriz.length;i++){
+        for (int j = 0;j < matriz[0].length;j++){
+            matriz[i][j]=0;
+            //definindo a diagonal principal e somando os elementos dela
+            if(i==j){
+                matriz[i][j]=impar;
+                somaDP = somaDP+matriz[i][j];
             }
+            System.out.printf("%d\t",matriz[i][j]);
+            impar+=2;
         }
+        System.out.printf("\n");
     }
+        System.out.println("A soma da diagonal princiapal da matriz é = " +somaDP);
     }
 }
